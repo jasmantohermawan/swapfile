cbccb86b624f173a0ef0316bfcd89650fe4ff1e1
callmejasman <98158275+callmejasman@users.noreply.github.com>
2022-02-14 23:16:05 +0700

    Create swapfile.sh

---
+++ swapfile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Adding Swap Size
+echo "Set Capacity: (e.g. 1G or greater)"
+read capacity
+sudo fallocate -l $capacity /swapfile
+
+# Change Permissions For Root Only
+sudo chmod 600 /swapfile
+
+# Create Swap Area on Swap File
+sudo mkswap /swapfile
+
+# Enable Permanent Swap on System
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Change swappiness Close to 0
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+
+# Finish
+echo "To know the effect, please restart the server."
