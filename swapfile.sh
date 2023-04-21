5a534bc609191b588e39da9794d07a878250a8b4
callmejasman <98158275+callmejasman@users.noreply.github.com>
2023-04-21 23:52:20 +0700

    Update swapfile.sh

--- swapfile.sh
+++ swapfile.sh
@@ -17,10 +17,10 @@ sudo mkswap /swapfile
 
 # Enable Permanent Swap on System
 sudo cp /etc/fstab /etc/fstab.bak
-echo '/swapfile none swap sw 0 0' >> /etc/fstab
+sudo echo '/swapfile none swap sw 0 0' >> /etc/fstab
 
 # Change swappiness Close to 0
-echo 'vm.swappiness=10' >> /etc/sysctl.conf
+sudo echo 'vm.swappiness=10' >> /etc/sysctl.conf
 
 # Finish
 echo "Installation Success"
